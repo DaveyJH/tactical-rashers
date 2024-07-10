8f445416bee9e20e7c8d237b8028a3233375e92f
DaveyJH <daveyj.dh@googlemail.com>
2024-07-10 17:51:48 +0100

    fix: Hide profile link for signed out users

--- src/components/nav/NavBarNavigation.js
+++ src/components/nav/NavBarNavigation.js
@@ -28,7 +28,7 @@ const NavBarNavigation = () => {
               <Nav className="me-auto align-items-md-center">
                 <NavBarLink exact to="/" textContent="Game feed" active border />
                 {currentUser && <GamesDropdown />}
-                <NavBarLink to="/profile" textContent="Profile" active border />
+                {currentUser && <NavBarLink to="/profile" textContent="Profile" active border />}
                 <NavBarLink to="/rules" textContent="Rules" active border />
                 {currentUser ? (
                   <NavBarLink to="/" textContent="Sign out" border signOut />
